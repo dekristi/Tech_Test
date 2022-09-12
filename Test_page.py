d17a08800785302d443e4cfa78f69daa42f82d21
dekristi <kkristine737@gmail.com>
2022-09-12 20:53:06 +0300

    Exercises Day4

--- Test_page.py
+++ Test_page.py
@@ -1,1 +1,13 @@
 print("I am so happy!")
+
+ask_name = input("What is your name ? ")
+
+user_age = input(f'{ask_name}, how old you are?')
+import datetime
+current_year = datetime.datetime.now().year
+
+age = int(user_age)
+
+hundred = int(current_year) - age + 100
+
+print(f'{ask_name} in {hundred} you will be 100 years old!')
