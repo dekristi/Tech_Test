6548b99327a1d879921d730344526e08f2afb8d9
dekristi <kkristine737@gmail.com>
2022-09-15 15:45:54 +0300

    Exercises_Day5

--- Exercises_Day5.py
+++ Exercises_Day5.py
@@ -1,28 +1,28 @@
 # Exercise Nr 1 'FizzBuzz'
 
-# range_list = ''
+range_list = ''
 
-# for i in range(101):
-#     if i % 5 == 0:
-#         range_list += 'Fizz, '
-#     elif i % 7 == 0:
-#         range_list += 'Buzz, '
-#     elif i % 7 == 0 and i % 5 == 0:
-#         range_list += 'FizzBuzz, '
-#     elif not (i % 7 ==0 and i % 5 == 0):
-#         range_list += str(i) + ', '
-# print(range_list)
+for i in range(101):
+    if i % 5 == 0:
+        range_list += 'Fizz, '
+    elif i % 7 == 0:
+        range_list += 'Buzz, '
+    elif i % 7 == 0 and i % 5 == 0:
+        range_list += 'FizzBuzz, '
+    elif not (i % 7 ==0 and i % 5 == 0):
+        range_list += str(i) + ', '
+print(range_list)
 
 ## Exercise Nr. 2 'Christmas tree'
 
-# ask = input("Please enter the hight of the tree! ")
+ask = input("Please enter the hight of the tree! ")
 
-# height = int(round(float(ask), 0))
-# stars = '*'
-# space = ' '
+height = int(round(float(ask), 0))
+stars = '*'
+space = ' '
 
-# for i in range(height + 1):
-#     print(space * (height - i - 1) + stars * (2 * i + 1))
+for i in range(height + 1):
+    print(space * (height - i - 1) + stars * (2 * i + 1))
 
 ### Exercise Nr 3 'Prime numbers'
 
