ae11c40b88f82882311a9891033f5e177d021a72
dekristi <kkristine737@gmail.com>
2022-09-25 19:17:07 +0300

    HM_23.09.2022

--- test.py
+++ test.py
@@ -1,48 +1,23 @@
-from turtle import clear
-num_list = []
-prime_num = False
+prime_list = [2,]
+num = int(input('How many prime numbers do you want to display? '))
+start_num = 1
+
 while True:
-    numbers = int(input('How much first prime numbers you want to know? '))
-    max_num = numbers**3
-    clear()
+    prime_list_length = len(prime_list) 
 
+    for i in range(2, start_num):
+        if start_num % i == 0 and start_num in prime_list:
+            # prime_list = [n.replace("start_num", "") for n in prime_list]
+            prime_list = prime_list[:-1]
+
+        if start_num % i == 0:
+            break
     
-    for i in range(2, max_num):
-        if i > 0:
-            for num in range(2, max_num):
-                               
-                if i % num == 0 and i != num:
-                    break
-                if i <= num:
-                    break
-           
-                else:
-                    if i not in num_list:
-                        num_list.append(i)
-#                 # if i % num != 0 and i % 2 != 0:
-#                 #     if i not in num_list:
-#                 #         num_list.append(i)
-    print(num_list)
-    print(f'First {numbers} prime numbers are: {num_list[:numbers]}')
-    break
-# Program to check if a number is prime or not
-
-# num = 21
-
-# flag = False
-
-# # prime numbers are greater than 1
-# if num > 1:
-#     # check for factors
-#     for i in range(2, num):
-#         if (num % i) == 0:
-#             # if factor is found, set flag to True
-#             flag = True
-#             # break out of loop
-#             break
-
-# # check if flag is True
-# if flag:
-#     print(num, "is not a prime number")
-# else:
-#     print(num, "is a prime number")
+        else:
+            if start_num not in prime_list:
+                prime_list.append(start_num)
+    if prime_list_length < num:
+        start_num += 1
+    if prime_list_length == num:
+        print(prime_list)
+        break
