837c03a27e5b2021d03ff91724561c93f62d3f62
dekristi <kkristine737@gmail.com>
2022-09-27 21:10:48 +0300

    Tuples & Sets

--- test.py
+++ test.py
@@ -33,3 +33,36 @@ def get_min_avg_max (seq: tuple):
 
 min_avg_max = get_min_avg_max([0,10,1,9])
 print(min_avg_max)
+
+# 3. Is there a pangram?
+
+# Write a function is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz')
+
+# that returns True when the text parameter contains all the letters passed in an alphabet.
+# We return False otherwise
+
+# pangram - sentence, word string containing all letters of the alphabet - https://en.wikipedia.org/wiki/Pangram
+
+# We ignore spaces and believe that uppercase is as valid as lowercase, i. here A and a -> a
+
+# print(is_pangram("The five boxing wizards jump quickly")) -> True
+# print(is_pangram("Not a pangram")) -> False
+
+# Bonus: test it also on Latvian alphabet:
+# a_lv = 'aābcčdeēfgģhiījkķlļmnņoprsštuūvzž'
+
+# print(is_pangram('Tfū, čeh, džungļos blīkšķ, zvaņģim jācērp!', alphabet=a_lv)) -> True
+
+# Bonus: test it also on Lithuanian alphabet:
+# a_lt = 'aąbcčdeęėfghiįyjklmnoprsštuųūvzž' # see if this correct
+
+# some languages have perfect pangrams, some do not
+# perfect pangram - a pangram that uses every letter of the alphabet at just once
+
+def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
+    spl_text = set(text.lower().split())
+    spl_alph = set(alphabet.split())
+    return spl_alph.issubset(spl_text)
+
+my_text = is_pangram("The five boxing wizards jump quickly")
+print(f'is_pangram {my_text}')
