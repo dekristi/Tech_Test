03543fd7337b384735b1e3374ca1dab588f57c7f
dekristi <kkristine737@gmail.com>
2022-09-29 15:00:28 +0300

    HW Sets 29.09

---
+++ HM_Sets.py
@@ -0,0 +1,37 @@
+# write a function of 3 arguments all strings
+# function should return lexigraphically ordered string of unique characters
+# these unique characters must be present in BOTH  of the first two strings 
+# BUT not in the third "badstring"
+
+# example:
+# "abcf", "fab", "boo"  returns -> "af" 
+# because "a" is in both "abc" and "fab" but not in "boo"
+# same goes for "f" it is present in both "good strings" but not in "badstring"
+# notice "b" is not in the result because it is in the badstring.
+
+# slightly harder way would be to use loops and if statements to check each character
+# the easy way is to use sets and set operations 😊
+
+# either approach is acceptable
+
+ ## 1.One-liner with list
+
+def ordered_string(seq1, seq2, seq3):
+    bad_string = seq3
+    return [i for i in seq1 if i in seq2 and i not in bad_string]
+    
+## 2 String  
+
+def ordered_string(seq1, seq2, seq3):
+    empty_string = ''
+    my_list = [i for i in seq1 if i in seq2 and i not in seq3]
+    empty_string = ''.join(my_list)
+    return empty_string
+
+### Set operations
+
+def ordered_string(seq1, seq2, seq3):
+    return (set(seq1) & set(seq2)) - set(seq3)
+
+my_string = ordered_string('abcf', 'fab', 'boo')
+print(my_string)
--- Tuples_Sets.py
+++ Tuples_Sets.py
@@ -1,3 +1,10 @@
+from statistics import mean
+def get_min_avg_max (seq: tuple):
+    return min(seq), mean(seq), max(seq)
+
+min_avg_max = get_min_avg_max([0,10,1,9])
+print(min_avg_max)
+
 # 2. Common Elements
 # Write a function that returns a tuple with common elements in three sequences. Inputs can be list, tuple, string.
 
@@ -8,26 +15,22 @@
 # get_common_elements("abc", ['a', 'b'], ('b', 'c')) -> ('b',) # we return a tuple with a single element 
 
 # # remember that we can convert strings to set with set(mystring), and set to tuple with tuple(myset)
-
+def common_elements(seq1, seq2, seq3):
+    return set(seq1) & set(seq2) & set(seq3)
 # 2. b For those with some experience 
 
 # BONUS:  make a function that can handle an arbitrary number of input sequences
 # so function which takes any number of sequences and returns a tuple with common elements
 # get_common_elements(seq1, seq2, seq3, seq4, seq5, seq6, seq7) etc :), so just like print takes any number of values
 
-def common_elements(seq1, seq2, seq3):
-    return set(seq1) & set(seq2) & set(seq3)
+def get_common_elements_unlimited(*seq):
+    if len(seq) == 0:
+        return tuple()
+    return tuple(set(seq[0]).intersection(*seq[1:]))
 
 
 my_elements = common_elements(['a', 'b'], ('b', 'c'), "abc")
 print(my_elements)
-
-from statistics import mean
-def get_min_avg_max (seq: tuple):
-    return min(seq), mean(seq), max(seq)
-
-min_avg_max = get_min_avg_max([0,10,1,9])
-print(min_avg_max)
 
 # 3. Is there a pangram?
 
@@ -55,9 +58,8 @@ print(min_avg_max)
 # perfect pangram - a pangram that uses every letter of the alphabet at just once
 
 def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
-    spl_text = set(text.lower())
-    spl_alph = set(alphabet)
-    return spl_alph.issubset(spl_text)
+    
+    return set(alphabet).issubset(set(text.lower()))
 
 my_text = is_pangram("The five boxing wizards jump quickly", alphabet='abcdefghijklmnopqrstuvwxyz')
 print(f'is_pangram {my_text}')
