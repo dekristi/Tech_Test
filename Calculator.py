dd240ba4566780362d22c6e294f73ae02ffce795
dekristi <kkristine737@gmail.com>
2022-12-02 19:00:39 +0200

    Class Calculator (simple)

--- Calculator.py
+++ Calculator.py
@@ -1,48 +1,9 @@
-# # Simple Calculator
-# def add(n1, n2):
-#     return n1 + n2
-
-# def divide(n1, n2):
-#     return n1 / n2
-
-# def substract(n1, n2):
-#     return n1 - n2
-
-# def multiply(n1, n2):
-#     return n1 * n2
-
-# calc_list = {
-#     "+": add,
-#     "/": divide,
-#     "-": substract,
-#     "*": multiply
-# }
-# def calculator():
-#     num1 = float(input("What is the first number? "))
-#     for key in calc_list:
-#         print(key)
-
-#     while True:
-#         operation = input('Please select an operator: ')
-#         num2 = float(input("What is the next number? "))
-#         oper = calc_list[operation]
-#         answer = oper(num1, num2)
-#         print(f"{num1} {operation} {num2} = {answer}")
-#         question = input(f"Type 'y' to continue calculating with {answer}, or type'n' to exit, or type 's' to start new calculator: ")
-#         if question == 'y':
-#             num1 = answer
-#             continue
-#         elif question == 's':
-#             calculator()
-#         else:
-#             break
-
-# calculator()
+# Simple Calculator
 
 class Calculator:
+
     def __init__(self):
-        self.num1 = float(input("What is the first number? "))
-        self.num2 = float(input("What is the next number? "))
+        self.calc_list = ['+', '-', '*', '/']
         
     def substruction(self):
         return self.num1 - self.num2
@@ -56,13 +17,69 @@ class Calculator:
     def divide(self):
         return self.num1 / self.num2
 
-    def calculation(self, n1, n2):
-        self.calc_opertors = ['+', '-', '*', '/']
-        self.operation = input(f"Please select an operation {self.calc_operators} ")
+    def calculation(self, num1=0, num2=0):
+        self.num1 = num1
+        self.num2 = num2
+        self.num1 = float(input("What is the first number? "))
+        while True:
+            for i in self.calc_list:
+                print(i)
+            self.operation = input(f"Please select an operation: ")
+            self.num2 = float(input("What is the next number? "))
+            if self.operation == '+':
+                result = Calculator.sum(self)
+                print(f"{self.num1} + {self.num2} = {result}")
+            elif self.operation == '-':
+                result = Calculator.substruction(self)
+                print(f"{self.num1} - {self.num2} = {result}")
+            elif self.operation == '/':
+                result = Calculator.divide(self)
+                print(f"{self.num1} / {self.num2} = {result}")
+            else:
+                result = Calculator.multiply(self)
+                print(f"{self.num1} * {self.num2} = {result}")
+            
+            question = input(f"Type 'y' to continue calculating with {result}, or type 'q' to exit, or type 's' to start new calculator: ")
+            if question == 'y':
+                self.num1 = result
+                continue
+            elif question == 's':
+                Calculator.calculation(self, num1=0, num2=0)
+            else:
+                break
+
 
 if __name__ == "__main__":
     my_calc= Calculator()
-    my_calc.calculation(5, 3)
+    my_calc.calculation()
 
+## VERY simple calculator
 
+# class Calculator:
 
+#     def __init__(self):
+#         self.calc_list = ['+', '-', '*', '/']
+        
+#     def substruction(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#         return self.num1 - self.num2
+    
+#     def sum(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#         return self.num1 + self.num2
+    
+#     def multiply(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#         return self.num1 * self.num2
+
+#     def divide(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#         return self.num1 / self.num2
+
+# if __name__ == "__main__":
+#     my_calc= Calculator()
+#     print(my_calc.multiply(2, 3))
